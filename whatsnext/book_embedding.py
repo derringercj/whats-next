e386e86b99beabf17fea1d288a4ac56f21942d23
Casey Derringer <derringercj@gmail.com>
2025-07-21 17:37:01 -0400

    Wrote function to load books, also generated model embeddings in main.

--- whatsnext/book_embedding.py
+++ whatsnext/book_embedding.py
@@ -1,10 +1,20 @@
 from sentence_transformers import SentenceTransformer
 
-def load_books():
-    pass
+import re
 
+def load_books():
+    # Opening the saved books text file, reading the data, and splitting it on the title delimiter
+    with open("data/formattedbookobjects.txt", "r") as file:
+        text = file.read()
+        books = re.split(r'(?=Title:)', text)
+
+    return books
 def main():
-    pass
+    books = load_books()
+    # Generating embeddings for each book string in the text file
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    embeddings = model.encode(sentences=books, batch_size=32, show_progress_bar=True)
+    print(embeddings.shape)
 
 if __name__ == "__main__":
     main()
