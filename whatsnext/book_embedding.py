bf29ac6c58a3abd57ba5d5d990419091f86a1f8a
Casey Derringer <derringercj@gmail.com>
2025-07-23 22:19:59 -0400

    Fixed embedding dict saving, started rec engine

--- whatsnext/book_embedding.py
+++ whatsnext/book_embedding.py
@@ -2,6 +2,7 @@ from sentence_transformers import SentenceTransformer
 
 import constants as constant
 
+import gzip
 import json
 import re
 
@@ -15,16 +16,22 @@ def load_books():
 
 def create_embedding_lookup(books, embeddings):
     i = 0
-    book_vector_dict = {}
+    book_vector_dicts = []
     for book in books:
-        book_vector_dict[book.split("\n")[0]] = embeddings[i].tolist()
+        book_vector = {}
+        book_vector["title"] = book.split("\n")[0]
+        book_vector["vector"] = embeddings[i].tolist()
+        book_vector_dicts.append(book_vector)
         i += 1
     
-    return book_vector_dict
+    return book_vector_dicts
 
 def save_dictionary(book_dict):
-    with open(constant.BOOKS_EMBEDDING_PAIRS_FILENAME, "w") as file:
-        json.dump(book_dict, file, sort_keys=True)
+    with gzip.open(constant.BOOKS_EMBEDDING_PAIRS_FILENAME, "wb") as file:
+        content = []
+        for dict in book_dict:
+            content.append((json.dumps(dict)+'\n').encode('utf-8'))
+        file.writelines(content)
     
 def main():
     books = load_books()
--- whatsnext/rec_engine.py
+++ whatsnext/rec_engine.py
@@ -1,25 +1,41 @@
 from sentence_transformers import SentenceTransformer
 
+import constants as constant
 import numpy as np
 
+import faiss
+import gzip
 import jsonlines
 
 def load_data():
-    with open("data/blahblahblah.json", "r") as file:
+    with jsonlines.Reader(gzip.open(constant.BOOKS_EMBEDDING_PAIRS_FILENAME, "rt")) as reader:
         pass
 
 def main():
 
+    i = 0
     books_db = load_data()
+    for book_title in books_db["title"]:
+        print(book_title)
+        i += 1
+        if i == 5:
+            break
 
     model = SentenceTransformer("all-MiniLM-L6-v2")
 
     print("Please enter a title:")
     title = input()
     
-    running = True
-    while running:
-        pass
+
+    # running = True
+    # while running:
+    #     similarities_list = []
+    #     title_similarity_pair = {}
+    #     title_embedding = books_db[title]
+    #     for book in books_db:
+    #         similarity = model.similarity(title_embedding, books_db[book])
+    #         title_similarity_pair[title] = similarity
+    #         similarities_list.append(title_similarity_pair)
 
 
 
